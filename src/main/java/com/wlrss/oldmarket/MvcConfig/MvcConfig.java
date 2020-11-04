c847720852745eea1e3bb091cf3348cb454c1b45
xi_nian <1617171230@qq.com>
2020-11-04 12:37:55 +0800

    二手交易平台

--- src/main/java/com/wlrss/oldmarket/MvcConfig/MvcConfig.java
+++ src/main/java/com/wlrss/oldmarket/MvcConfig/MvcConfig.java
@@ -99,6 +99,7 @@ public class MvcConfig implements WebMvcConfigurer {
                 ).excludePathPatterns(
                         "/admin/**",
                         "/css/**",
+                        "/common/**",
                         "/img/**",
                         "/js/**",
                         "/vendor/**",
